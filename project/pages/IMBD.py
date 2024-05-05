72b521551b343bf6b1220da115a75958f4ec6036
atomicGandi <lore.ruelasg@gmail.com>
2024-05-05 03:29:55 -0600

    se agregó una grafica, al resto le falta corregir metodos filters - para parsear bien la info

--- project/pages/IMBD.py
+++ project/pages/IMBD.py
@@ -1,5 +1,7 @@
 import pandas as pd
 import matplotlib.pyplot as plt
+import matplotlib.colors as mcolors
+import numpy as np
 import streamlit as st
 import json
 from sklearn.metrics.pairwise import cosine_similarity
@@ -42,6 +44,12 @@ def createCleanDataset():
     dataset.reset_index(drop=True, inplace=True)
     dataset = dataset.sort_index() 
 
+    #cambiar tipo de fecha a datetime
+    dataset['release_date'] = pd.to_datetime(dataset['release_date'])
+
+    #cambiar genres lrg
+    #dataset['production_companies'] = dataset['production_companies'].apply(lambda x: json.loads(x) if isinstance(x, str) else x)
+
     #borrar NaN de release_date
     dataset = dataset.dropna(subset=['release_date'])
 
@@ -63,6 +71,13 @@ def createCleanDataset():
 
     return dataset
 
+    # Definir la función para extraer los géneros
+def extraer_generos(json_list) -> list:
+    nombres = []
+    for json in json_list:
+        nombres.append(json['name'])
+    return nombres
+
 def obtener_peliculas_similares(dataset, movie, top_n=5):
     movie_index = dataset.index[dataset['title']==movie].tolist()[0]
     # Convertir listas en vectores binarios usando MultiLabelBinarizer
@@ -78,6 +93,144 @@ def obtener_peliculas_similares(dataset, movie, top_n=5):
     _peliculasRecomendadas = dataset.iloc[similar_movies_indices]
 
     return _peliculasRecomendadas
+
+# GENRE BY TITLE
+
+def extract_genres(data, movie_title):
+    #data['genres'] = data['genres'].apply(lambda x: json.loads(x) if isinstance(x, str) else x)
+    # Ensure the 'genres' column is a list of dictionaries if it is stored as a string
+    if isinstance(data['genres'].iloc[0], str):
+        data['genres'] = data['genres'].apply(lambda x: json.loads(x))
+    
+    # Filter the DataFrame for the given movie title
+    movie_row = data[data['original_title'] == movie_title]
+    
+    # Check if any movie was found
+    if not movie_row.empty:
+        # Extract genre list from the first (and should be only) row
+        genres_list = movie_row.iloc[0]['genres']
+        # Collect all genre names into a list
+        
+        genre_names = [genre['name'] for genre in genres_list]
+        return genre_names
+    else:
+        return "Movie not found"
+    
+# Define functions to filter and retrieve top movies by genre
+def filter_by_genre(data, genre_name):
+    return data[data['genres'].apply(lambda g: any(genre['name'] == genre_name for genre in g))]
+
+def get_top_movies_by_rating(data, genre_name):
+    genre_movies = filter_by_genre(data, genre_name)
+    top_movies = genre_movies.sort_values(by='vote_average', ascending=False).head(5)
+    return top_movies
+
+def top_genre_votes(data, genre_name):
+    top_movies = get_top_movies_by_rating(data, genre_name)
+    plt.figure(figsize=(15, 6))  # Wider plot for better layout
+    colors = list(mcolors.TABLEAU_COLORS)  # Get a set of color names
+
+    # Create a scatter plot
+    for i, (index, row) in enumerate(top_movies.iterrows()):
+        plt.scatter(row['vote_count'], row['vote_average'], color=colors[i % len(colors)], s=100, label=row['original_title'])
+
+    plt.xlabel('Vote Count')
+    plt.ylabel('Average Vote')
+    plt.title(f'Top 5 {genre_name} Movies by Average Vote')
+    plt.legend(title="Movie Titles", loc='upper left', bbox_to_anchor=(1, 1))  # Move legend outside of the plot
+    plt.grid(True)
+    st.pyplot(plt)
+
+    # YEAR BY TITLE
+
+    # Filter movies by year
+def filter_movies_by_year(data, year):
+    return data[data['release_date'].dt.year == year]
+
+# Get top 5 movies by vote average
+def get_top_movies_by_vote(data, year):
+    year_movies = filter_movies_by_year(data, year)
+    top_movies = year_movies.sort_values(by='vote_average', ascending=False).head(5)
+    return top_movies
+
+    # Function to extract the release year of a movie by its title
+def extract_release_year(data, movie_title):
+    # Filter the DataFrame for the given movie title
+    movie_row = data[data['original_title'] == movie_title]
+
+    # Check if the movie was found
+    if not movie_row.empty:
+        # Extract the release year from the 'release_date' column
+        release_year = movie_row['release_date'].dt.year.iloc[0]
+        return release_year
+    else:
+        return "Movie not found"
+
+def top_by_year(data, year):
+    top_movies = get_top_movies_by_vote(data, year)
+    plt.figure(figsize=(15, 8))  # Adjusted for better display of horizontal bars
+    plt.barh(top_movies['original_title'], top_movies['vote_average'], color='skyblue')
+    plt.ylabel('Movie Title')
+    plt.xlabel('Average Vote')
+    plt.title(f'Top 5 Movies of {year} by Average Vote')
+
+    # Setting y-axis scale to have a tick every 0.25 points
+    vote_min = top_movies['vote_average'].min() - 0.25  # slightly lower to add breathing room
+    vote_max = top_movies['vote_average'].max() + 0.25  # slightly higher to add breathing room
+    plt.xticks(ticks=np.arange(int(vote_min), int(vote_max) + 0.25, 0.25))  # adjust ticks on x-axis for clarity
+
+    st.pyplot(plt)
+
+    # PRODUCTION COMPANY BY TITLE
+    
+def extract_production_company(data, movie_title):
+     # Find the movie row based on its title
+    movie_row = data[data['original_title'] == movie_title]
+    
+    # Check if any movie was found
+    if movie_row.empty:
+        return "Movie not found"
+    
+    # Extract the production company list from the first (should be only) row
+    production_companies_list = movie_row.iloc[0]['production_companies']
+    
+    # Debug print to check what the actual data looks like
+    #print("Debug - Production Companies Data:", production_companies_list)
+
+    return production_companies_list
+    
+    
+# Filter movies by production company
+
+def filter_movies_by_company(data, company_name):
+    data['production_companies'] = data['production_companies'].apply(lambda x: json.loads(x) if isinstance(x, str) else x)
+    return data[data['production_companies'].apply(lambda companies: any(company['name'] == company_name for company in companies))] # lrg
+
+# Get top 5 movies by vote average
+def get_top_movies_by_company(data, company_name):
+    company_movies = filter_movies_by_company(data, company_name)
+    top_movies = company_movies.sort_values(by='vote_average', ascending=False).head(5)
+    return top_movies[['original_title', 'vote_average', 'vote_count']]  # Including vote_count for the scatter plot
+
+
+def top_by_company(data, company_name):
+    top_movies = get_top_movies_by_company(data, company_name)
+    plt.figure(figsize=(15, 6))  # Wider plot
+    colors = list(mcolors.TABLEAU_COLORS)  # Get a set of color names from Matplotlib's tableau color set
+
+    # Create a scatter plot
+    for i, (index, row) in enumerate(top_movies.iterrows()):
+        plt.scatter(row['vote_count'], row['vote_average'], color=colors[i % len(colors)], s=100, label=row['original_title'])
+
+    plt.xlabel('Vote Count')
+    plt.ylabel('Average Vote')
+    plt.title(f'Top 5 Movies by {company_name} by Average Vote')
+    plt.legend(title="Movie Titles", loc='upper left', bbox_to_anchor=(1, 1))  # Move legend outside of the plot
+    plt.grid(True)
+    st.pyplot(plt)
+
+
+
 
 if __name__ == "__main__":
     ## Creamos nuestro dataframe
@@ -118,5 +271,28 @@ if __name__ == "__main__":
             st.write(nombresPeliculas[4])
             st.image(imagesPeliculas[4])
             st.write(f'Mas información: {linkPaginaIMDB[4]}')
-    st.header("Curiosidades sobre las peliculas")
+        
+    # lrg genre = extract_genres(movies_data, pelicula_base)
 
+    release_year = extract_release_year(movies_data, pelicula_base)
+    prod_company = extract_production_company(movies_data, pelicula_base)
+
+    st.header("Top 5 Peliculas de <> con Mayor Popularidad")
+
+    st.header("Top 5 Peliculas del año {release_year} con Mayor Promedio de Votos ")
+    top_by_year(movies_data, release_year)
+
+    st.header("Top 5 Peliculas con Mayor Promedio de Votos Producida por <> ")
+    print(prod_company)
+    for cp in prod_company:
+        top_by_company(movies_data, cp)
+
+""" lrg    for g in genre:
+        st.header("Top 5 Peliculas d {g} con Mayor Promedio de Votos ")
+        top_genre_votes(movies_data, g) """
+
+
+
+
+
+    
